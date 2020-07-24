9fc93d649c7dfe0132c2e78fe5d2cffc2707b870
Robin Donatello <rdonatello@csuchico.edu>
2020-07-24 15:27:39 -0700

    split weeks into separate files

--- notes/convert_slides_to_pdf.R
+++ notes/convert_slides_to_pdf.R
@@ -3,4 +3,3 @@
 pagedown::chrome_print("file:///C:/Github/MATH130/notes/01_intro_slides.html", verbose=T)
 pagedown::chrome_print("file:///C:/Github/MATH130/notes/02_setup_slides.html", verbose=T)
 pagedown::chrome_print("file:///C:/Github/MATH130/notes/03_rmd_slides.html", verbose=T)
-pagedown::chrome_print("file:///C:/Github/MATH130/notes/04_r_slides.html", verbose=T)
