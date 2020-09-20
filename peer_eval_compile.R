70a93bfd400ec09aba0f049e861fb5c803ddefde
Robin Donatello <rdonatello@csuchico.edu>
2020-09-20 15:17:25 -0700

    typo in lesson09 fix eda instructions and add pr grade compile script

--- peer_eval_compile.R
+++ peer_eval_compile.R
@@ -1,27 +1,20 @@
-library(googlesheets)
+library(googlesheets4)  # https://googlesheets4.tidyverse.org/
 library(dplyr)
 
-# Open up google and ask for permissions
-gs_auth() 
-
-# Get the sheet info for the spreadsheet you want
-sheet_info <- gs_title("130 EDA Peer Grading Rubric (Responses)")
+# New users may have to authorize googlesheets4 to access the GDrive API first. 
 
 # read the data into a data frame
-pr_raw <- sheet_info %>% gs_read()
+pr_raw <- read_sheet("https://docs.google.com/spreadsheets/d/17VhpLa9AZOkeOUYtxyog5NwXL389HLKP7v8VqVmDdSU/edit#gid=1340211106")
 
 # rename to usable names
 names(pr_raw) <- c("timestamp", "author", "repro", "desc", "univ1", "univ2", "bivar", "summary", "org", "email", "blank", "upload")
 
 # drop all but this semester
-pr_raw$date <- as.Date(strptime(pr_raw$timestamp, format="%m/%d/%Y"))
-pr <- pr_raw %>% filter(as.Date(date) > "2019-03-02" & as.Date(date) < "2019-06-01") 
+pr <- pr_raw %>% filter(as.Date(timestamp) > "2019-03-02" & as.Date(timestamp) < "2019-06-01") 
 
-# Number of reviews per person -- for grading peer reviews
-table(pr$email)
 
 # Identify instructor review vs peer review
-pr$IR <- ifelse(grepl("rdonatello", pr$email), "Instructor Review", "Peer Review")
+pr$IR <- ifelse(grepl("rdonatello|nlytal|eroualdes|klgray", pr$email), "Instructor Review", "Peer Review")
 pr$IR[is.na(pr$IR)] <- "Peer Review"
 
 # average score per review
@@ -41,29 +34,14 @@ fg <- pr %>% group_by(author, IR) %>%
         print(n=Inf)
 
 
+#######################################
+#       THINGS TO GRADE
+#######################################
 
-# write out
-write.table(table(pr$email), "nreviews_per_person.txt", sep="\t")
-grades_for_ed <- fg %>% select(author, `Peer Review`)
-write.table(grades_for_ed, "final_grades.txt", sep="\t")
+# Number of reviews per person -- give credit for reviewing peer work
+table(pr$email)
 
-# Compile numeric reviews to return to author as a zip file
-setwd("C:/Box/Teaching/130/08 S19")
-ppl <- unique(pr$author)
-ppl.names <- paste0("EDA_", gsub(" ", "" ,ppl))
-all.files <- list.files(path="EDA/", full.names = TRUE)
-
-for (p in 1:length(ppl)){
-  filename <- paste0(ppl.names[p], "_scores.csv")
-  person <- pr %>% filter(author==ppl[p]) %>% 
-                   select(IR, repro, desc, univ1, bivar, org, score) %>%
-                   write.csv(filename, row.names = FALSE)
-  zip(paste0(ppl.names[p], ".zip"), files = c(
-    all.files[grep(ppl.names[p], all.files, ignore.case=TRUE)], #not working
-    filename))
-  file.remove(filename)
-}
-
-
+# Full set of grades: Instructor, Peer & weighted grade
+fg
 
 
